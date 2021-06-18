dc41f2e756d1b685de3ea0b0f3c7bccc7bbb2008
hoyeon96 <hoyen96@naver.com>
2021-06-18 17:33:08 +0900

    삭제 및 클릭 시 토글 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import React, {useState, useRef} from 'react';
+
+const Globalstyle = createGlobalStyle`
+body{
+  background : #e9ecef;
+
+}
+`;
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '복습하기',
+      done: true
+    },
+    {
+      id: 2,
+      text: '주석달기',
+      done: true
+    },
+    {
+      id: 3,
+      text: '코드 해석 및 분석하기',
+      done: false
+    },
+    {
+      id: 4,
+      text: '기능 추가로 구현해보기',
+      done: false
+    }
+  ]);
+
+  const nextId = useRef(5);
+
+  const addTodo = text => {
+    setTodos([
+      ...todos,
+      {id: nextId.current, 
+        Todo: text, 
+        done: false},
+    ]);
+  };
+
+  const onRemove = id => {
+    console.log("리무브 실행");
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const onToggle = id => {
+    setTodos(
+      todos.map(
+        todo => todo.id === id?
+        {...todo, done :!todo.done} : todo)); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Globalstyle />
+    <TodoTemplate>
+      <TodoHead />
+      <TodoList 
+        todos={todos}
+        onRemove={onRemove}
+        onToggle={onToggle}
+      />
+      <TodoCreate onAddTodo={addTodo}/>
+    </TodoTemplate>
+    </>
+    )
 }
 
 export default App;
