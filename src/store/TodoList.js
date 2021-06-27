04bcf31153b3e16ea7b887c9787991dce251370c
hoyeon96 <hoyen96@naver.com>
2021-06-27 21:20:03 +0900

    초기 데이터 배열 설정

---
+++ src/store/TodoList.js
@@ -0,0 +1,59 @@
+
+const initialState = {
+    todos: [
+      {
+        id: 1,
+        date: "2021-05-05",
+        text: "어린이날 놀기",
+        done: false,
+      },
+      {
+        id: 2,
+        date: "2021-05-20",
+        text: "5월20일 뒹굴거리기",
+        done: false,
+      },
+      {
+        id: 3,
+        date: "2021-06-07",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 4,
+        date: "2021-06-08",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 5,
+        date: "2021-06-09",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 6,
+        date: "2021-06-10",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 7,
+        date: "2021-07-04",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 8,
+        date: "2021-07-10",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+      {
+        id: 9,
+        date: "2021-07-11",
+        text: "리액트공부하기2021년",
+        done: false,
+      },
+    ]
+  };
